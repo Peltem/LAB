4ef921cec083e16762eda7af15f3897830252d59
Peltem <Igor200462283@gmail.com>
2023-10-09 14:00:43 +0700

    LAB7

---
+++ ИС(ПРО)41/Pz 20/animeFEST/animeFEST/class/Organizer.cs
@@ -0,0 +1,12 @@
+namespace animeFEST.class;
+
+public class Organizer
+{
+    public class Sotrudniki
+    {
+        public int IDHum { get; set; }
+        public string FIO { get; set; }
+        public string Dni { get; set; }
+        public int dolzhnost { get; set; }
+    }
+}
